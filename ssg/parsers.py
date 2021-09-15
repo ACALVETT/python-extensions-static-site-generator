7a32f5e421477e2e93b8dbcd47b40677b9a3549f
ACALVETT <andrew@calvett.co.uk>
2021-09-15 11:16:56 +0100

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
-from ssg.hooks import hooks
+from ssg import hooks
 
 
 class Parser:
